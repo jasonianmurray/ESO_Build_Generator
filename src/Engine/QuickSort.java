755694bd5f2dac82db2991186037ca3dd658168d
Jason Ian Murray <murray.jason.ian@gmail.com>
2017-11-27 02:53:58 +0000

    Implemented a recursive quicksort that will be used by the GA (during selection)

--- src/Engine/QuickSort.java
+++ src/Engine/QuickSort.java
@@ -13,11 +13,35 @@
  * for selection.
  */
 package Engine;
+import Agent.Agent;
 
 public class QuickSort {
+	int p;
+	public QuickSort(Agent [] pop, int left, int right) {
+		p = partition(pop, left, right);
+		new QuickSort(pop, left, p-1);
+		new QuickSort(pop, p+1, right);
+	}
 	
+	private int partition(Agent [] pop, int left, int right) {
+		double v = pop[right].getFitness();
+		int pLeft = left, pRight = right;
+		
+		while(pLeft < pRight) {
+			while(pop[pLeft].getFitness() < v) pLeft++;
+			while(pop[pRight].getFitness() >= v && pRight > left) pRight--;
+			if(pLeft < pRight) swap(pop, pLeft, pRight);
+		}
+		
+		swap(pop, pLeft, right);
+		return pLeft;
+	}
+	
+	
+	private void swap(Agent [] pop, int pLeft, int pRight) {
+		Agent temp = pop[pLeft];
+		pop[pLeft] = pop[pRight];
+		pop[pRight] = temp;
+	}
 }
 
-class Partition {
-	
-}
